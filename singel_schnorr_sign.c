f4afe5ffc63f1aa890b5df1bc7b17546828650d3
iceboy926 <anddy926@163.com>
2019-11-18 10:35:03 +0800

    schnorr sign

---
+++ singel_schnorr_sign.c
@@ -0,0 +1,9 @@
+//
+//  schnorr_sign.c
+//  testSM2
+//
+//  Created by zuoyongyong on 2019/11/15.
+//  Copyright © 2019年 zuoyongyong. All rights reserved.
+//
+
+#include "singel_schnorr_sign.h"
---
+++ singel_schnorr_sign.h
@@ -0,0 +1,15 @@
+
+//
+//  schnorr_sign.h
+//  testSM2
+//
+//  Created by zuoyongyong on 2019/11/15.
+//  Copyright © 2019年 zuoyongyong. All rights reserved.
+//
+
+#ifndef schnorr_sign_h
+#define schnorr_sign_h
+
+#include <stdio.h>
+
+#endif /* schnorr_sign_h */

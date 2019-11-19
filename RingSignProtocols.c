de6d743f78d714dc5559b311c6445efe0dc76f6e
iceboy926 <anddy926@163.com>
2019-11-19 14:05:00 +0800

    perform signature

--- RingSignProtocols.c
+++ RingSignProtocols.c
@@ -7,3 +7,35 @@
 //
 
 #include "RingSignProtocols.h"
+
+void ringGenkeyPair(unsigned char *prikey, unsigned int *prikeylen, unsigned char *pubkey, unsigned int *pubkeylen)
+{
+    //generate signer keypair (ai, Ai)  user include (0,1,2, ..., n-1)
+    // generate other n-1 pubkey A0,A1,A2, ..Ai-1,..Ai+1..,An-1
+}
+
+void ringSignGen(unsigned char *plain, unsigned int plainlen, unsigned char *prikey, unsigned int prikeylen, unsigned char *allPubkey, unsigned int allPubkeylen, unsigned char *sign, unsigned int *signlen)
+{
+    
+    //1、signer generate  n-1 random,  s0, s1, s2, ...si-1, si+1, ...sn  basepoint G  , si is been computed  in step 4
+    
+    //2 signer generate  a random k, compute kG = P  , assume P = si*G + ci*Ai;
+    
+    //3、according to formual c(i) = Hash(m|| (si-1*G + ci-1*Ai-1)) i = {0,1,2, ..n-1}     c0 = Hash(m||(sn-1*G + cn-1*An-1))  m is plaintext
+    // as we know: ci+1 = Hash(m|| (si*G + ci*Ai)) = Hash(m|| P); then continue calculate  {ci+1,ci+2, .., cn-1,c0, c1, c2,...ci}
+    // then perform an Ring-Sign
+    
+    //4、 according to ci, signer compute si = k - ci*ai
+    
+    //5、 perform an signature is {c0, s0, s1,...sn-1}
+    
+}
+
+void ringVerifySign(unsigned char* plain, unsigned int plainlen, unsigned char *allPubkey, unsigned int allPubkeylen, unsigned char *sign, unsigned int signlen)
+{
+    //1、 convert sign to {c0, s0, s1, ..., sn-1}    convert all pubkey to {A0, A1,....,An-2, An-1}
+    
+    
+    //2、
+    
+}

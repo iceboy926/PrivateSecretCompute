2582bffbbff632175b1feb77d9306b10490e14d0
iceboy926 <anddy926@163.com>
2019-12-18 09:55:04 +0800

    add monero ring sign && keyimage

---
+++ monero_ring_sign.h
@@ -0,0 +1,14 @@
+//
+//  monero_ring_sign.h
+//  testSM2
+//
+//  Created by zuoyongyong on 2019/12/17.
+//  Copyright © 2019年 zuoyongyong. All rights reserved.
+//
+
+#ifndef monero_ring_sign_h
+#define monero_ring_sign_h
+
+#include <stdio.h>
+
+#endif /* monero_ring_sign_h */

1c9825e639712c6d87aa2863a769948ce3347ab3
iceboy926 <anddy926@163.com>
2020-01-21 14:03:20 +0800

    update monero sign

---
+++ randlib.c
@@ -0,0 +1,9 @@
+//
+//  randlib.c
+//  testSM2
+//
+//  Created by zuoyongyong on 2020/1/2.
+//  Copyright © 2020年 zuoyongyong. All rights reserved.
+//
+
+#include "randlib.h"
---
+++ randlib.h
@@ -0,0 +1,14 @@
+//
+//  randlib.h
+//  testSM2
+//
+//  Created by zuoyongyong on 2020/1/2.
+//  Copyright © 2020年 zuoyongyong. All rights reserved.
+//
+
+#ifndef randlib_h
+#define randlib_h
+
+#include <stdio.h>
+
+#endif /* randlib_h */

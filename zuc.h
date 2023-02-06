ca915d4b3356997e9bdfba5c08477af19ffcf01f
iceboy926 <anddy926@163.com>
2023-02-06 11:28:33 +0800

    Add files via upload

---
+++ zuc.h
@@ -0,0 +1,23 @@
+//
+//  search_sym_enc.h
+//  testSM2
+//
+//  Created by zuoyongyong on 2019/11/5.
+//  Copyright © 2019年 zuoyongyong. All rights reserved.
+//
+
+#ifndef zuc_stream_enc_h
+#define zuc_stream_enc_h
+
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+    
+
+int test_zuc_main();
+    
+
+#endif /* zuc_stream_enc_h */
